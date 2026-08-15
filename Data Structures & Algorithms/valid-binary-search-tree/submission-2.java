3762a84d174b0542916bf11a3f4ad66286374810
Advait Moghe <advaitmoghe2006@gmail.com>
2026-08-15 16:30:41 -0700

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean valid(TreeNode root, long left, long right) {
+        if (root == null) {
+            return true;
+        }
+
+        if (left < root.val && right > root.val) {
+             return valid(root.left, left, root.val) && valid(root.right, root.val, right);
+        }
+        return false;
+    }
+}
