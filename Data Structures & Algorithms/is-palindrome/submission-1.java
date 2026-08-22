27e97f3c8960bff672d05b78804d8f0cebaaa790
Advait Moghe <advaitmoghe2006@gmail.com>
2026-08-21 21:18:32 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+                continue;
+            };
+
+            if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+                continue;
+            };
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
